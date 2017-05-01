2dbc3085e0fe4fee74db90cd25779f084d770fa3
bene2929 <benedikt.kantz@htl-klu.at>
2017-05-01 20:27:19 +0200

    init

---
+++ main.js
@@ -0,0 +1,91 @@
+/**
+ * Created by Benedikt on 01/05/2017.
+ */
+var shows = [];
+function Show(episodes, show) {
+    this.show = show;
+    this.episodes = episodes;
+}
+$("body").ready(function () {
+    $("button").button();
+
+    $("#search").on('input', function () {
+
+        console.log("typed..");
+        var input = $("#search").val();
+        var url = "http://api.tvmaze.com/search/shows?q=" + encodeURI(input);
+        console.log(url);
+        $.getJSON(url, null, function (data) {
+            console.log(data);
+            var html = "";
+            for (var i = 0; i < data.length; i++) {
+                html += "<div class=\"show_search\">";
+                if (data[i].show.image) {
+                    html += "<img  height='20px' src='" + data[i].show.image.medium + "'>";
+                }
+                html += data[i].show.name + "(" + getYearFromDate(data[i].show.premiered) + ")  <a id='add_show~" + data[i].show.id + "' class='add_show_link' >Add</a></div>\n";
+            }
+            $("#search_res").html(html);
+            $(".add_show_link").click(function (obj) {
+                console.log(obj.target.id);
+                var id = obj.target.id.split("~")[1];
+
+                var contained = false;
+                for (var i_sh = 0; i < shows.length; i++) {
+                    if (shows[i][0].id == parseInt(id)) {
+                        contained = true;
+                        break;
+                    }
+                }
+                if (!contained) {
+                    var url = "http://api.tvmaze.com/shows/" + id;
+
+                    $.getJSON(url, null, function (data_show) {
+                        url += "/episodes"
+                        $.getJSON(url, null, function (data_episode) {
+                            console.log(data_show);
+
+                            console.log(data_episode);
+                            shows.push(new Show(data_episode, data_show));
+                            updateShowList();
+                        });
+                    });
+                }
+            });
+        });
+    });
+});
+function updateShowList() {
+    var html = "";
+    for (var i = 0; i < shows.length; i++) {
+        html += "<div class=\"show_display\">";
+        if (shows[i].show.image) {
+            html += "<img  class='display_img' height='20px' src='" + shows[i].show.image.medium + "'>";
+        }
+        html += shows[i].show.name + "(" + getYearFromDate(shows[i].show.premiered) + ")</div>\n";
+    }
+    $("#added").html(html);
+    recalculateNumber();
+}
+function recalculateNumber() {
+    var total_min = 0;
+    for (var is = 0; is < shows.length; is++) {
+        for (var ie = 0; ie < shows[is].episodes.length; ie++) {
+            total_min += shows[is].episodes[ie].runtime;
+        }
+    }
+    var hrs = Math.floor(total_min / 60);
+    var min = total_min % 60;
+    if(min<10){
+        min="0"+min;
+    }
+    $("#result").html(hrs + ":" + min);
+}
+function getYearFromDate(date) {
+    if (date) {
+
+        return parseInt(date.split("-")[0]);
+    } else {
+        return "-";
+    }
+}
